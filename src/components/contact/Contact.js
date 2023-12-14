70bd44f97e324e0e543b8ab4a3fa5cb6503374fa
nhussein2026 <naser738nas@gmail.com>
2023-12-14 18:44:09 +0300

    feat(contact, experience): implement the initial design

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,10 +1,15 @@
-
-
+import styles from './Contact.module.css'
+import { GrLinkedinOption } from "react-icons/gr";
+import { HiOutlineMail } from "react-icons/hi";
 const Contact = () => {
 
     return (
-        <div>
-
+        <div className={styles.parent}>
+            <h1>Get In Touch</h1>
+            <div className={styles.contact}>
+                <span className={styles.social}><a href='https://www.linkedin.com/in/nhussein2026/' target='_blank' rel='noopener noreferrer'><GrLinkedinOption /></a> </span>
+                <span className={styles.social}><a href='mailto:naser738nas@gmail.com'><HiOutlineMail /></a></span>
+            </div>
         </div>
     )
 }
--- src/components/experience/Experience.js
+++ src/components/experience/Experience.js
@@ -1,11 +1,14 @@
-import { TbDice3Filled } from "react-icons/tb"
-
-
+import styles from './Experience.module.css'
 const Experience = () => {
 
-    return(
-        <div>
-
+    return (
+        <div className={styles.parent}>
+            <h1>My Experience</h1>
+            <div className={styles.experience}>
+                <div className={styles.first}></div>
+                <div className={styles.second}></div>
+                <div className={styles.third}></div>
+            </div>
         </div>
     )
 }
