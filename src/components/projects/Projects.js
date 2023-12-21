0d46453194398f5d260e16f4807ac84b267aa8aa
nhussein2026 <naser738nas@gmail.com>
2023-12-22 00:43:34 +0300

    feat(projects): Insert one project detail beside fix styling

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -5,7 +5,7 @@ import { TfiGithub } from "react-icons/tfi";
 const Projects = () => {
     return (
         <div>
-            <span className={styles.title}><span className={styles.smallTitle}>03. </span> Eager for New Ventures!</span>
+            <span className={styles.title}><span className={styles.smallTitle}>03. </span> Passionate about Innovation!</span>
             <div className={styles.projects}>
                 {/* Project items */}
                 <div className={styles.project}>
@@ -35,7 +35,22 @@ const Projects = () => {
                 <div className={styles.project}>
                     <div className={styles.projectNav}>
                         <span className={styles.icon}><LuFolder /></span>
-                        <span className={styles.icon}><TfiGithub /></span>
+                        <span className={styles.icon}><a href='https://github.com/nhussein2026/quiz-management-system' target='_blank' rel='noopener noreferrer' ><TfiGithub /></a></span>
+                    </div>
+                    <div className={styles.projectDes}>
+                        <span className={styles.ProjectTitle}>
+                            Quiz Management System
+                        </span>
+                        <p className={styles.des}>
+                        Designed and implemented a Java-based Quiz System for 
+                        online quiz creation, management, and participation.
+                         Utilized MySQL for seamless data storage and retrieval.
+                        </p>
+                        <span className={styles.skills}></span>
+                    </div>
+                    <div className={styles.projecFooter}>
+                        <span>Java</span>
+                        <span>MySQL</span>
                     </div>
                 </div>
                 <div className={styles.project}>
