2f2b948b4fd1803fd02d0b57984fcf75fc6f9589
nhussein2026 <naser738nas@gmail.com>
2024-01-06 13:08:02 +0300

    fix(experience): edit the internal info and design

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -4,8 +4,24 @@ const About = () => {
 
     return (
         <div className={styles.about}>
-            <span className={styles.contactSpan}><span className={styles.smallSpan}>01. </span> About Me</span>
-            
+            <span className={styles.smalltextSpan}><span className={styles.smallSpan}>01. </span> About Me</span>
+            <div className={styles.aboutMe}>
+                <h2 className={styles.name}>Nasser Hussein</h2>
+                <p className={styles.location}>Istanbul, Turkey</p>
+                <p className={styles.bio}>Software Engineering student and web developer passionate about crafting dynamic and responsive websites that elevate user experiences. ‍</p>
+                <div className={styles.skills}>
+                    <h3>Skills</h3>
+                    <ul>
+                    <li>JavaScript</li>
+                    <li>MySQL</li>
+                    <li>Node.js</li>
+                    <li>React</li>
+                    <li>Express.js</li>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    </ul>
+                </div>
+          </div>
         </div>
     )
 }
--- src/components/experience/Experience.js
+++ src/components/experience/Experience.js
@@ -3,14 +3,42 @@ const Experience = () => {
 
     return (
         <div className={styles.parent}>
-            <span className={styles.contactSpan}><span className={styles.smallSpan}>02. </span> Eager for New Ventures!</span>
-            <h1>My Experience</h1>
-            <div className={styles.experience}>
-                <div className={styles.first}></div>
-                <div className={styles.second}></div>
-                <div className={styles.third}></div>
+        <span className={styles.smalltextSpan}><span className={styles.smallSpan}>02. </span> Eager for New Ventures!</span>
+        <h1>My Experience</h1>
+        <div className={styles.experience}>
+            <div className={styles.first}>
+                <h4>Full-Stack Web Development Bootcamp</h4>
+                <p>Woz U, June 2023 - October 2023</p>
+                <ul>
+                    <li>Completed a comprehensive four-month bootcamp focusing on full-stack web development.</li>
+                    <li>Mastered technologies such as JavaScript, MySQL, Node.js, React, Express.js, HTML, and CSS.</li>
+                    <li>Collaborated with a team to create a fully functional book review website, showcasing skills in user registration, book reviews, and comment sections.</li>
+                </ul>
+                <p>Relevant Skills: JavaScript, MySQL, Node.js, React, Express.js, HTML, CSS</p>
+            </div>
+            <div className={styles.second}>
+                <h4>Software Engineering Intern</h4>
+                <p>EARTech Information Technology, December 2023 - Present</p>
+                <ul>
+                    <li>Engaged in real-world software engineering projects, gaining hands-on experience in DevOps practices.</li>
+                    <li>Utilized skills in JavaScript, HTML, and CSS to contribute to the development of dynamic and responsive websites.</li>
+                    <li>Continuously involved in learning and adapting to new challenges in the software development industry.</li>
+                </ul>
+                <p>Relevant Skills: DevOps, JavaScript, HTML, CSS</p>
+            </div>
+            <div className={styles.third}>
+                <h4>Core Team Member</h4>
+                <p>Google Developer Student Club, October 2023 - Present</p>
+                <ul>
+                    <li>Actively participated as a core team member, contributing to various development projects.</li>
+                    <li>Applied expertise in JavaScript, Express.js, React, and Node.js to enhance the functionality of web applications.</li>
+                    <li>Thrives on embracing new challenges and learning opportunities within the software development field.</li>
+                </ul>
+                <p>Relevant Skills: JavaScript, Express.js, React, Node.js</p>
             </div>
         </div>
+    </div>
+    
     )
 }
 
